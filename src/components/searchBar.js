42db8214825d833ff3a69c973d01a2f6d4cbfd98
Osvaldo Saldana <osvaldo.saldana99@gmail.com>
2019-01-23 21:11:57 -0700

    put together fetchPostWithQuery action to receive queried posts

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -7,7 +7,6 @@ import { withRouter } from 'react-router-dom';
 class SearchBar extends Component {
 
     handleFormSubmit = function({query}) {
-        console.log('trying to handle submit for query', query)
         this.props.onSubmit(query);
     }
 
